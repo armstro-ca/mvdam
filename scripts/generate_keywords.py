81ae936a7322f01bb01fc2a1573b861fbf86a271
James Armstrong <j@armstro.ca>
2023-10-25 15:25:15 -0700

    Adding helper script

--- scripts/generate_keywords.py
+++ scripts/generate_keywords.py
@@ -1,9 +1,14 @@
+import sys
+
 import pandas as pd
 import random
 
 import nltk
 from nltk.corpus import words
 import random
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
 
 # Download the words dataset
 nltk.download('words')
@@ -19,15 +24,17 @@ random_words = english_words[:100]
 
 
 # Load Dataframe
-with open('assets.csv', 'r') as f:
+with open(input_file, 'r') as f:
     df = pd.read_csv(f)
+
 
 # Function to generate random keywords
 def generate_keywords():
     return ', '.join(random.sample(random_words, k=random.randint(3, 10)))
 
+
 # Apply the function to create a new 'Keywords' column
 df['Keywords'] = df.apply(lambda row: generate_keywords(), axis=1)
 
 # Save the DataFrame
-df.to_csv('assets_with_keywords.csv', index=False, encoding='utf-8')
+df.to_csv(output_file, index=False, encoding='utf-8')
